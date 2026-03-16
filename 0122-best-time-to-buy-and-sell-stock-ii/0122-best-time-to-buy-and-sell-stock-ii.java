9e2e5891d179e95fe1b22529ecf64073ca0609cd
Vivek Kumar <168298365+vivek-kumar-github@users.noreply.github.com>
2026-03-16 23:30:09 +0530

    0122-BEST-TIME-TO-BUY-AND-SELL-STOCK-II Stats: Time: 0 ms (100%), Space: 46.5 MB (50.78%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        //greedy approach
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                profit += prices[i] - prices[i - 1];
+        //peak valley approach
+        int n = prices.length, peak = prices[0], valley = prices[0], i = 0, profit = 0;
+        while (i < n - 1) {
+            while (i < n - 1 && prices[i] >= prices[i + 1]) {
+                i++;
             }
+            valley = prices[i];
+            while (i < n - 1 && prices[i] <= prices[i + 1]) {
+                i++;
+            }
+            peak = prices[i];
+            profit += peak - valley;
         }
         return profit;
     }
