89c7cb1e3c01191d2ffbecb96ae45fe72caa031b
Vivek Kumar <168298365+vivek-kumar-github@users.noreply.github.com>
2026-03-19 10:48:22 +0530

    0041-FIRST-MISSING-POSITIVE Stats: Time: 14 ms (25.7%), Space: 93.5 MB (6.6%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -3,22 +3,14 @@ class Solution {
         //using extra space
         int n = nums.length;
         HashSet<Integer> list = new HashSet<>();
-        int min = 1;
-        int max = 1;
         for (int num : nums) {
-            if (num < min) {
-                min = num;
-            } else if (num > max) {
-                max = num;
-            }
             list.add(num);
         }
-        if (min <= 0) min = 1;
-        for (int i = min; i <= max; i++) {
+        for (int i = 1; i <= n + 1; i++) {
             if (!list.contains(i)) {
                 return i;
             }
         }
-        return max + 1;
+        return 1;
     }
 }
