22cacd27c2b98c456b118fe8afeed16181bd945a
Vivek Kumar <168298365+vivek-kumar-github@users.noreply.github.com>
2026-03-21 11:38:25 +0530

    0392-IS-SUBSEQUENCE Stats: Time: 1 ms (94.83%), Space: 42.6 MB (88.36%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
+        int x = s.length(), v = t.length();
         int i = 0, j = 0;
-        while(i < s.length() && j < t.length()){
-            if(s.charAt(i) == t.charAt(j)){
-                i++;
-                j++;
-            }else{
+        while (i < x && j < v) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++; j++;
+            } else {
                 j++;
             }
         }
-        return i == s.length();
+        if (i == x) {
+            return true;
+        }
+        return false;
     }
 }
