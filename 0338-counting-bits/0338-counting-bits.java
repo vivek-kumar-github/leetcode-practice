46f24c73ca811e9f61c9354cce583fbfbb5bd952
Vivek Kumar <168298365+vivek-kumar-github@users.noreply.github.com>
2026-04-01 20:54:18 +0530

    0338-COUNTING-BITS Stats: Time: 1 ms (99.92%), Space: 48.2 MB (98.62%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,18 +1,10 @@
 class Solution {
     public int[] countBits(int n) {
-        //using brian kernighan algo
-        int[] table = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
-            table[i] = brian(i);
+        //using DP and bit manipulation
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
         }
-        return table;
-    }
-    public int brian(int n) {
-        int count = 0;
-        while (n != 0) {
-            n = n & (n - 1);
-            count++;
-        }
-        return count;
+        return ans;
     }
 }
