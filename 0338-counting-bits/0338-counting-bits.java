ffb50a7a378b2255ba3182890754af75e3e1dd4c
Vivek Kumar <168298365+vivek-kumar-github@users.noreply.github.com>
2026-04-01 22:09:46 +0530

    0338-COUNTING-BITS Stats: Time: 2 ms (96.61%), Space: 48.4 MB (95.31%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] countBits(int n) {
-        //using DP and bit manipulation
+        //using DP with last set bit Brian Kernighan's trick
         int[] ans = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            ans[i] = ans[i >> 1] + (i & 1);
+            ans[i] = ans[i & (i - 1)] + 1;
         }
         return ans;
     }
