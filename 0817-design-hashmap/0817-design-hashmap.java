18c18186b55053dcfab8d5efbe84ba9960c9d35e
Vivek Kumar <viveksingh599999@gmail.com>
2026-04-06 09:07:38 +0530

    0817-DESIGN-HASHMAP Stats: Time: 16 ms (82.62%), Space: 49.3 MB (84.12%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.java
+++ 0817-design-hashmap/0817-design-hashmap.java
@@ -1,22 +1,61 @@
 class MyHashMap {
-    //brute force direct address array
-    private int[] data;
+    //using Open Addressing (Linear Probing)
+    private static final int SIZE = 20011;
+    private static final int EMPTY = -1;
+    private static final int TOMBSTONE = -2;
+    private int[] keys;
+    private int[] values;
 
     public MyHashMap() {
-        data = new int[1000001];
-        Arrays.fill(data, -1);
+        keys = new int[SIZE];
+        values = new int[SIZE];
+        Arrays.fill(keys, EMPTY);
+    }
+
+    private int hash(int key) {
+        return key % SIZE;
     }
     
     public void put(int key, int value) {
-        data[key] = value;
+        int index = hash(key);
+        int firstTombStone = -1;
+        while (keys[index] != EMPTY) {
+            if (keys[index] == key) {
+                values[index] = value;
+                return;
+            }
+            if (keys[index] == TOMBSTONE && firstTombStone == -1) {
+                firstTombStone = index;
+            }
+            index = (index + 1) % SIZE;
+        }
+        if (firstTombStone != -1) {
+            index = firstTombStone;
+        }
+        keys[index] = key;
+        values[index] = value;
     }
     
     public int get(int key) {
-        return data[key];
+        int index = hash(key);
+        while (keys[index] != EMPTY) {
+            if (keys[index] == key) {
+                return values[index];
+            }
+            index = (index + 1) % SIZE;
+        }
+        return -1;
     }
     
     public void remove(int key) {
-        data[key] = -1;
+        int index = hash(key);
+        while (keys[index] != EMPTY) {
+            if (keys[index] == key) {
+                keys[index] = TOMBSTONE;
+                return;
+            }
+            index = (index + 1) % SIZE;
+        }
     }
 }
 
