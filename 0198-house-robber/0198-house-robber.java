865ade179ba9551a5d8a771d22aaee98abbbc854
Vivek Kumar <viveksingh599999@gmail.com>
2026-07-28 09:00:44 +0530

    0198-HOUSE-ROBBER Stats: Time: 0 ms (100%), Space: 42.7 MB (75.55%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,11 @@
 class Solution {
-    public int rec(int i, int n, int[] dp, int[] nums) {
-        if (i >= n) return 0;
-        if (dp[i] != -1) return dp[i];
-        int profit = 0;
-        profit = nums[i] + rec(i + 2, n, dp, nums);
-        profit = Math.max(profit, rec(i + 1, n, dp, nums));
-        return dp[i] = profit;
-    }
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        rec(0, n, dp, nums);
+        int[] dp = new int[n + 2];
+        Arrays.fill(dp, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+        }
         return dp[0];
     }
 }
