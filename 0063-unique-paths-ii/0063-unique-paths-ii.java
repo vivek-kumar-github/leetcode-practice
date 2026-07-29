6cf5e8dde2f9402e6d799438258b006449088db8
Vivek Kumar <viveksingh599999@gmail.com>
2026-07-29 09:22:14 +0530

    0063-UNIQUE-PATHS-II Stats: Time: 1 ms (16.84%), Space: 43.9 MB (10.99%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,19 +1,17 @@
 class Solution {
-    public int rec(int i, int j, int[][] dp, int n, int m, int[][] grid) {
-        if (i == n - 1 && j == m - 1) return 1;
-        if (dp[i][j] != -1) return dp[i][j];
-        int ways = 0;
-        if (j + 1 < m && grid[i][j + 1] != 1) ways += rec(i, j + 1, dp, n, m, grid);
-        if (i + 1 < n && grid[i + 1][j] != 1) ways += rec(i + 1, j, dp, n, m, grid);
-        return dp[i][j] = ways;
-    }
     public int uniquePathsWithObstacles(int[][] grid) {
         int n = grid.length, m = grid[0].length;
-        if(grid[0][0]==1) return 0;
+        if (grid[0][0] == 1) return 0;
         int[][] dp = new int[n][m];
-        for (int i =0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
+        if (grid[n - 1][m - 1] != 1) dp[n - 1][m - 1] = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (grid[i][j] != 1) {
+                    if ((i + 1) < n) dp[i][j] += dp[i + 1][j];
+                    if ((j + 1) < m) dp[i][j] += dp[i][j + 1];
+                }
+            }
         }
-        return rec(0, 0, dp, n, m, grid);
+        return dp[0][0];
     }
 }
